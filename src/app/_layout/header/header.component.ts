b0ea007a543ffd5fc1d4870d5fcfc6e4024f22e5
Mani Teja <manitejachalavadi765@gmail.com>
2025-05-25 23:39:51 +0530

    Added Header

--- src/app/_layout/header/header.component.ts
+++ src/app/_layout/header/header.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() toggleSidenav = new EventEmitter<void>();
+  isDarkTheme = false;
 
+  setTheme(isDark: boolean) {
+    this.isDarkTheme = isDark;
+    document.body.classList.toggle('dark-theme', isDark);
+  }
 }
--- src/app/_layout/side-nav/side-nav.component.ts
+++ src/app/_layout/side-nav/side-nav.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './side-nav.component.scss'
 })
 export class SideNavComponent {
-
+  
 }
